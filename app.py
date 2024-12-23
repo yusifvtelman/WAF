8de5c1275d5aa33fd43a9f78f1a4422e528b0d3f
yusifvtelman <yusifvtelman@gmail.com>
2024-12-23 17:17:24 +0400

    test

--- app.py
+++ app.py
@@ -7,8 +7,14 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/login", response_class=HTMLResponse)
-async def login(request: Request):
+async def loginPage(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
+
+@app.post("/login")
+async def login(request: Request, username: str = Form(...), password: str = Form(...)):
+    if username == "admin" and password == "admin":
+        return {"message": "Login Successful"}
+    return {"error": "Invalid Credentials"}
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
